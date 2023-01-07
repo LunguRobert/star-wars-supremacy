752fbd098c1cb4c956349e4538569aab857d0bea
LunguRobert <lungurobert1997@gmail.com>
2023-01-07 14:02:53 +0200

    change ws-wss

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
   } from "react-router-dom";
 import { w3cwebsocket } from 'websocket';
 
-const client = new w3cwebsocket('ws://82.78.233.77:3001')
+const client = new w3cwebsocket('wss://star-wars-supremacy-server.onrender.com:10000')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
