3583d1015ebcce3d53518cd7bc0162706cf61a5b
LunguRobert <lungurobert1997@gmail.com>
2023-01-08 22:23:12 +0200

    console log

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import {
 import { w3cwebsocket } from 'websocket';
 
 const client = new w3cwebsocket('wss://star-wars-supremacy-server.onrender.com')
-console.log(client.readyState);
 function stayAwake(){
+    console.log(client.readyState);
     if (client.readyState === 1) {
         client.send(JSON.stringify({type: 'stay_awake'}));
         console.log('stay awake')
