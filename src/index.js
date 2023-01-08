453dfc3f51c5f50f8f3063ffbff851f6d6f322f7
LunguRobert <lungurobert1997@gmail.com>
2023-01-08 23:02:54 +0200

    ping method update

--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ import {
 import { w3cwebsocket } from 'websocket';
 
 const client = new w3cwebsocket('wss://star-wars-supremacy-server.onrender.com')
+
+client.onopen = () => {
+    setInterval(stayAwake, 20000);
+    stayAwake();
+};
+
 function stayAwake(){
     console.log(client.readyState);
-    if (client.readyState === 1) {
-        client.send(JSON.stringify({type: 'stay_awake'}));
-        console.log('stay awake')
-    }
+    client.send(JSON.stringify({type: 'stay_awake'}));
+    console.log('stay awake')
 }
 
-setInterval(stayAwake,20000);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
