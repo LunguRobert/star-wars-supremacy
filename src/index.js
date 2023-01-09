4e1ba0e9a5d00402aeb197c53057491ddfc4148c
LunguRobert <99295585+LunguRobert@users.noreply.github.com>
2023-01-09 13:53:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {
   } from "react-router-dom";
 import { w3cwebsocket } from 'websocket';
 
-const client = new w3cwebsocket('wss://whimsical-kitten-e5dca1.netlify.app')
+const client = new w3cwebsocket('wss://supremacy-server.herokuapp.com')
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
